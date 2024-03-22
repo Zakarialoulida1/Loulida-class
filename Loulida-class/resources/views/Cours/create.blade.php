8d013bf1560b7cc92e8f38c2608d0d721ede6f67
zakaria loulida <zakarialoulida92@gmail.com>
2024-03-22 17:05:12 +0000

    cour disponible

---
+++ Loulida-class/resources/views/Cours/create.blade.php
@@ -0,0 +1,54 @@
+
+
+<x-app>
+
+
+    @if (session('success'))
+        <script>
+            swal("Success!", "{{ session('success') }}", "success");
+        </script>
+    @endif
+
+    <form method="POST" enctype="multipart/form-data" action="{{ route('cour.store') }}" class="m-4  max-w-md mx-auto">
+        @csrf
+        <h1 class="text-5xl text-center">Add Your lesson</h1>
+        <div class="relative z-0 w-full mb-5 group">
+            <input type="text" name="title" id="title"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" " required />
+            <label for="title"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Title</label>
+        </div>
+        <div class="relative z-0 w-full mb-5 group">
+            <textarea name="description" id="description"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" " required></textarea>
+            <label for="description"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Description</label>
+        </div>
+
+  
+        <div class="relative z-0 w-full mb-5 group">
+            <select name="categorie_id" id="categorie_id"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                required>
+                <option value="">Select a Cycle</option>
+                @foreach ($cycles as $cycle)
+                    <option value="{{ $cycle->id }}">{{ $cycle->name }}</option>
+                @endforeach
+            </select>
+            <label for="categorie_id"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Cycles</label>
+        </div>
+    
+      
+        <div class="relative z-0 w-full mb-5 group">
+      
+<label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
+<input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file">
+
+        </div>
+        <button type="submit"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+    </form>
+</x-app>
