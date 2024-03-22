779ebdd169380425f9eeeddd24e10b4a7cbbddbe
zakaria loulida <zakarialoulida92@gmail.com>
2024-03-22 17:17:25 +0000

    creation la relation pour Cycle Educative

---
+++ Loulida-class/app/Models/CycleEducative.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CycleEducative extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+     
+    ];
+    // public function cours()
+    // {
+    //     return $this->hasMany(Cours::class);
+    // }
+    public function matieres()
+    {
+        return $this->belongsToMany(Matiere::class, 'cycle_matiere');
+    }
+}
