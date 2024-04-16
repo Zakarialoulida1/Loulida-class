30c9ca8224aa162ec1b9b19bbac0474bfa404526
zakaria loulida <zakarialoulida92@gmail.com>
2024-04-16 23:49:50 +0100

    create a table for partners teachers

---
+++ Loulida-class/database/migrations/2024_04_09_141704_create_partners_table.php
@@ -0,0 +1,31 @@
+<?php
+// database/migrations/YYYY_MM_DD_create_partners_table.php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePartnersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('partners', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type',['Teacher','Investor']); // Indicates whether the partner is a teacher or investor
+            $table->string('phone')->nullable();
+            $table->string('Address');
+            $table->text('description')->nullable();
+            $table->string('cv')->nullable(); // Path to the CV file
+           $table->enum('status',['non_valide','validé'])->default('non_valide');  
+         
+           $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('partners');
+    }
+}
