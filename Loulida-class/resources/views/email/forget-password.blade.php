89921117f137bfc790015901507ee333e8fbc95a
zakaria loulida <zakarialoulida92@gmail.com>
2024-04-17 23:50:30 +0100

    email changed

--- Loulida-class/resources/views/email/forget-password.blade.php
+++ Loulida-class/resources/views/email/forget-password.blade.php
@@ -1,1 +1,33 @@
-<a href="{{route('password.reset',$token)}}"> Reset password</a>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-***********" crossorigin="anonymous" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link rel="stylesheet" href="<?php echo asset('css/app.css'); ?>" type="text/css">
+   
+</head>
+
+<body>
+<p class="text-red-400" >to reset your password click the link below  </p>
+
+    <a class="w-fit p-4 bg-    " href="{{route('password.reset',$token)}}"> Reset password</a>
+
+
+ 
+
+</body>
+
+
+
+</html>
+
