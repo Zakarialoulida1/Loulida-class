3a5604502ae5e988c51daffba2eb6f0cdce2bdb4
zakaria loulida <zakarialoulida92@gmail.com>
2024-04-18 23:48:58 +0100

    create formations model and their relations

---
+++ Loulida-class/app/Models/Formation.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Formation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'cycle_educative_id',
+        'available_place',
+        'price',
+        'duration_months',
+        'description',
+  'image',
+        // Add other fillable fields as needed
+    ];
+
+    public function cycleEducative()
+    {
+        return $this->belongsTo(CycleEducative::class);
+    }
+
+    public function matieres()
+    {
+        return $this->belongsToMany(Matiere::class);
+    }
+}
