16e6941b6285a28238e816abfc0975dac7bf8df0
zakaria loulida <zakarialoulida92@gmail.com>
2024-04-19 23:48:16 +0100

    adjust function to store partners

--- Loulida-class/app/Http/Controllers/PartnerController.php
+++ Loulida-class/app/Http/Controllers/PartnerController.php
@@ -12,43 +12,48 @@ class PartnerController extends Controller
 {
    
 
-   
-
     public function submitTeacherForm(Request $request)
     {
-
         $existingTeacherRequest = Partner::where('user_id', auth()->id())
-        ->where('type', 'teacher')
-        ->where('status', 'non_valide')
-        ->first();
+            ->where('type', 'teacher')
+            ->where('status', 'non_valide')
+            ->first();
 
-    if ($existingTeacherRequest) {
-        return redirect()->back()->with('error', 'You already have a pending request as a teacher.');
-    }
+        if ($existingTeacherRequest) {
+            return redirect()->back()->with('error', 'You already have a pending request as a teacher.');
+        }
 
         $request->validate([
             'Phone' => 'required',
             'Description' => 'required',
             'Address' => 'required',
-            'cv' => 'required|file|mimes:pdf|max:2048', 
-            // 2048 KB (2MB) maximum size
+            'cv' => 'required|file|mimes:pdf|max:2048',
+            'Social_Media' => 'nullable|array', // Validate as an array since it will have multiple values
+            'Social_Media.facebookName' => 'nullable|string',
+            'Social_Media.instagramName' => 'nullable|string',
+            'Social_Media.linkedinName' => 'nullable|string',
+            'matiere_id' => 'required',
         ]);
-
+      
         // File upload logic
         if ($request->hasFile('cv')) {
             $cv = $request->file('cv');
-            $cvName = time() . '.' . $cv->getClientOriginalExtension();
+            $cvName = time() . '.' . $cv->getClientOriginalName();
             $cv->move(public_path('cv'), $cvName); // Move the file to 'public/cv' directory
 
-            // Store the file name and form data in the database
+         // Store the file name and form data in the database
             Partner::create([
                 'type' => $request->type,
                 'phone' => $request->input('Phone'),
                 'description' => $request->input('Description'),
                 'cv' => $cvName,
-                'Address'=>$request->Address,
+                'Address' => $request->Address,
+                'Social_Media' => json_encode($request->input('Social_Media')) , // Store Social Media data as JSON
+                'matiere_id' => $request->input('matiere_id'), // Store profession
+                
                 'user_id' => auth()->id(), // Set the default status
             ]);
+            
         }
 
         return redirect()->back()->with('success', 'Your application as a teacher has been submitted successfully.');
